e2d5c545c697a4398e8236a6e325b28db5bc1d6e
akatsuki0354 <francogregorio2004@gmail.com>
2023-02-06 14:54:46 +0000

    first commit

---
+++ javascript/student/home-student.js
@@ -0,0 +1,51 @@
+let userlink = document.getElementById('userlink');
+let signoutlink = document.getElementById('signoutlink');
+let text = document.getElementById("text")
+let header = document.getElementById('hh');
+var currentUser = null;
+
+function getUsername(){
+    let KeepLogin = localStorage.getItem("KeepLogin");
+
+    if (KeepLogin == "yes"){
+        currentUser = JSON.parse(localStorage.getItem('user'));
+    }
+    else{
+        currentUser = JSON.parse(sessionStorage.getItem('user'));
+    }
+}
+
+function Signout(){
+    sessionStorage.removeItem('user');
+    localStorage.removeItem('user');
+    localStorage.removeItem('KeepLogin');
+    window.location = "studentlogin.html";
+}
+
+window.onload = function(){
+    getUsername();
+    if(currentUser == null){
+        userlink.innerText="create New Account";
+        userlink.classList.replace("nav-link","btn");
+        userlink.classList.add("btn-primary");
+        userlink.href = "signup.html";
+
+        signoutlink.innerText="Login";
+        signoutlink.classList.replace("nav-link","btn");
+        signoutlink.classList.add("btn-info");
+        signoutlink.href = "studentlogin.html";
+    }
+    else{
+        text.value = currentUser.name;
+        userlink.innerText = currentUser.stdNo;
+        header.innerText = currentUser.name;
+        userlink.classList.replace("nav-link","btn");
+        userlink.classList.add("btn-primary");
+        userlink.href = "profile.html";
+
+        signoutlink.innerText="Sign Out";
+        signoutlink.classList.replace("nav-link","btn");
+        signoutlink.classList.add("btn-info");
+        signoutlink.href = "javascript:Signout()";
+    }
+}
