d16c547ec8e91df6c3138de425e97df7c5b4c6b4
akatsuki0354 <francogregorio2004@gmail.com>
2023-03-02 18:12:22 +0800

    push

--- teacher/mscbot-main/mscbot/static/scripts/responses.js
+++ teacher/mscbot-main/mscbot/static/scripts/responses.js
@@ -1,3 +1,5 @@
+
+
 function getBotResponse(input) {
     //rock paper scissors
     if (input == "rock") {
@@ -8,6 +10,25 @@ function getBotResponse(input) {
         return "rock";
     }
 
+    if (input == "bakit ka absent") {
+        return "Kase po meron po ako lagnat";
+    } else if (input == "ahh ok pagaling ka") {
+        return "thank you po";
+    } else if (input == "your welcome") {
+        return "good bye";
+    } else if (input == "bakit") {
+        return "abesnt po ako nga yun";
+    }
+
+    if (input == "generate number") {
+        let randomNum = Math.floor(Math.random() * 10) + 1;
+        if (randomNum <= 5) {
+        return "The number is less than or equal to 5.";
+        } else {
+        return "The number is greater than 5.";
+        }
+    }
+
     // Simple responses
     if (input == "hello") {
         return "Hi";
@@ -16,4 +37,6 @@ function getBotResponse(input) {
     } else {
         return "Try asking something else!";
     }
+
+   
 }
