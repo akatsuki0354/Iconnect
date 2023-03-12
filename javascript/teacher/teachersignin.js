4f8a0b3276293499706ef600e585a370448b4510
akatsuki0354 <francogregorio2004@gmail.com>
2023-03-13 03:10:38 +0800

    ASAS

--- javascript/teacher/teachersignin.js
+++ javascript/teacher/teachersignin.js
@@ -3,7 +3,6 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase
 import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -16,43 +15,34 @@ const firebaseConfig = {
     appId: "1:937341367464:web:32573b0a9ef70099247e9d",
     measurementId: "G-D9QFV2F1QQ"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase();
-
-
 //  Reference
-
 const stdNo = document.getElementById('usernameInp');
 const password = document.getElementById('passInp');
 const submit = document.getElementById('submit');
-
 // AuthenticatorAttestationResponse
-
 function AuthenticateUser() {
     const dbRef = ref(db);
-
     let passwordregex = /[a-zA-Z0-9]{8,}/;
     let stdNoregex = /^[a-zA-Z0-9\-]{5,}$/;
-
     // Validate username and password input
     if (!stdNoregex.test(stdNo.value)) {
         stdNo.classList.add('invalid');
         alert("Please enter a valid username (minimum 5 characters).");
         return;
-    }else {
+    } else {
         stdNo.classList.remove('invalid');
     }
     if (!passwordregex.test(password.value)) {
         stdNo.classList.add('invalid');
         alert("Please enter a valid password (minimum 8 characters).");
         return;
-    }else {
+    } else {
         stdNo.classList.remove('invalid');
     }
-
     get(child(dbRef, "teacherlist/" + stdNo.value))
         .then((snapshot) => {
             if (snapshot.exists()) {
@@ -69,31 +59,21 @@ function AuthenticateUser() {
             }
         });
 }
-
 function decPass(dbpass) {
     var pass12 = CryptoJS.AES.decrypt(dbpass, password.value);
     return pass12.toString(CryptoJS.enc.Utf8);
 }
-
 // LOGIN
-
 function login(user) {
     let keepLoggedIn = document.getElementById('customSwitch1').checked;
     if (!keepLoggedIn) {
         sessionStorage.setItem('user', JSON.stringify(user));
         window.location = "teacherhome.html";
-
     }
-
     else {
         localStorage.setItem('KeepLogin', 'yes');
         localStorage.setItem('user', JSON.stringify(user));
         window.location = "teacherhome.html";
     }
-    
 }
-
 submit.addEventListener('click', AuthenticateUser);
-
-
-
